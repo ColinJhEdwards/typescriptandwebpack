4b327e81190fd74e307acf8ce86cb988a48e098c
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-11 20:10:46 -0700

    finished setting up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,21 @@ module.exports = {
     filename: "bigBoy.js",
     path: path.resolve(__dirname, "dist"),
   },
+  //   tells webpack that there will be generated source maps to work with
+  devtools: "inline-source-map",
+  //   tell webpack how to work with files in project
+  module: {
+    rules: [
+      {
+        // any files that end with .ts will follow this rule, in this case use ts-loader
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  //   which file extentions webpack adds to imports it finds
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
